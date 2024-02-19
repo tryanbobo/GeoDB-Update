ca29e68fcd02166f064ae949c59fd74b83f21b0b
tb1302 <tb1302@txstate.edu>
2024-02-19 10:04:54 -0600

    This methods adds features to feature classes a geoDB from a list of shpfiles. It does not remove anything.

--- main.py
+++ main.py
@@ -12,13 +12,15 @@ shpFiles = arcpy.ListFeatureClasses("*.shp")
 # iterate through shpfiles and update geoDB
 if shpFiles:
     for shp in shpFiles:
-
         shpPath = os.path.join(shpLocation, shp)
         # extract the feature class name by excluding extension
         featClassName = os.path.splitext(shp)[0]
         featClassPath = os.path.join(gdbPath, featClassName)
         # check if featclass in geoDG exist first only existing feature class are "updated"
         if arcpy.Exists(featClassPath):
+            # deletes features from the geDB that were removed from the shapefiles in the workspace
+            # arcpy.DeleteField_management(shpPath, os.path.join(gdbPath, featClassName))
+            # appends features from the geDB that were added to the shapefiles in the workspace
             arcpy.Append_management(shpPath, os.path.join(gdbPath, featClassName), "No_Test")
             # arcpy.FeatureClassToFeatureClass_conversion(shpPath, gdbPath, featClassName)
             print(f"Updated {featClassName} in geodatabase")
